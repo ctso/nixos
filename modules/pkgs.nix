4b6a434afe098f299e0de1dd4d03c9980d68a179
Chris Soyars <chris@soyars.com>
2025-12-25 01:32:03 -0800

    packages

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -6,10 +6,15 @@
   environment.systemPackages = with pkgs; [
     fzf
     git
+    go
     jq
     just
     kubectl
+    nodejs_24
+    php83
+    python314
     ripgrep
+    rustup
     wget
   ];
 }

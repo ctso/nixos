863b456800fe9a992f1b9fecfa4012421ada7a60
Chris Soyars <chris@soyars.com>
2025-12-25 12:45:56 -0800

    chore: add ghostty keybind for quick terminal

--- modules/home-manager/common/apps/ghostty/default.nix
+++ modules/home-manager/common/apps/ghostty/default.nix
@@ -9,6 +9,7 @@
       shell-integration-features = "cursor,title,sudo";
       font-family = "JetBrainsMono Nerd Font Mono";
       background-opacity = 0.95;
+      keybind = "global:cmd+backquote=toggle_quick_terminal";
     };
   } // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
     package = null;

70a876051c69463a3dfc3f0578afce7c3e64e228
Chris Soyars <chris@soyars.com>
2025-12-29 22:35:08 -0800

    chore(potatovim): rework lsp

--- potatovim/lua/plugins/lsp.lua
+++ potatovim/lua/plugins/lsp.lua
@@ -1,12 +1,61 @@
 return {
+  -- nvim-lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+    },
+    -- merge keymaps per server rather than overwriting them
+    opts_extend = { "servers.*.keys" },
+    opts = function()
+      return {
+        -- default settings for all servers
+        servers = {
+          ["*"] = {
+            keys = {
+              { "gd", vim.lsp.buf.definition, desc = "Goto definition" },
+              { "gr", vim.lsp.buf.references, desc = "References" },
+            },
+          },
+          -- per‑server settings (example)
+          lua_ls = {
+            settings = {
+              Lua = {
+                runtime = { version = "LuaJIT" },
+                diagnostics = { globals = { "vim" } },
+              },
+            },
+            keys = {
+              { "<leader>cl", vim.lsp.codelens.run, desc = "Run code lens" },
+            },
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      -- define configs via vim.lsp.config()
+      for server, conf in pairs(opts.servers) do
+          if server ~= "*" then
+            vim.lsp.config(server, conf)
+          else
+            vim.lsp.config("*", conf)  -- global defaults
+          end
+      end
+    end,
+  },
+
+  -- mason.nvim: installs external tools (formatters, linters, LSP servers)
   {
     "mason-org/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    opts_extend = { "ensure_installed" },
     opts = {
-      ensure_installed = { "stylua" },
+      ensure_installed = { "stylua", "shfmt" },  -- list any tools you want Mason to install
     },
     config = function(_, opts)
       require("mason").setup()
-      -- install specified tools
       local registry = require("mason-registry")
       registry.refresh(function()
         for _, tool in ipairs(opts.ensure_installed) do
@@ -18,13 +67,14 @@ return {
       end)
     end,
   },
+
+  -- mason-lspconfig: ensures servers are installed via Mason and calls vim.lsp.enable()
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = {
-        "lua_ls",
-        "stylua",
-      },
+      -- list of LSP servers to install
+      ensure_installed = { "lua_ls", "rust_analyzer" },
+      -- automatically call vim.lsp.enable() for installed servers (default true)
       automatic_enable = true,
     },
     dependencies = {
@@ -32,5 +82,5 @@ return {
       "mason-org/mason.nvim",
     },
   },
-  { "neovim/nvim-lspconfig" },
 }
+

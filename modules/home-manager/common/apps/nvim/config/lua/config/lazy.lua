86f84b7e99d1a7e5c0b7fa766b4ec3dab01042e0
Chris Soyars <chris@soyars.com>
2025-12-31 12:58:28 -0800

    fix(vim): load options first

--- modules/home-manager/common/apps/nvim/config/lua/config/lazy.lua
+++ modules/home-manager/common/apps/nvim/config/lua/config/lazy.lua
@@ -15,13 +15,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 -- Setup lazy.nvim
+-- Note: mapleader and options are set in config/options.lua which loads before this
 require("lazy").setup({
   spec = {
     -- import your plugins

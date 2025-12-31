45cc827b2a604242f1a1a97b55b30fb54cbc2610
Chris Soyars <chris@soyars.com>
2025-12-31 12:51:52 -0800

    chore(vim): add a butter banner

--- modules/home-manager/common/apps/nvim/config/lua/plugins/ui.lua
+++ modules/home-manager/common/apps/nvim/config/lua/plugins/ui.lua
@@ -140,4 +140,43 @@ return {
 			{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
 		},
 	},
+
+	-- snacks dashboard
+	{
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = {
+				enabled = true,
+				preset = {
+					header = [[
+██████╗ ██╗   ██╗████████╗████████╗███████╗██████╗ 
+██╔══██╗██║   ██║╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗
+██████╔╝██║   ██║   ██║      ██║   █████╗  ██████╔╝
+██╔══██╗██║   ██║   ██║      ██║   ██╔══╝  ██╔══██╗
+██████╔╝╚██████╔╝   ██║      ██║   ███████╗██║  ██║
+╚═════╝  ╚═════╝    ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝
+]],
+					keys = {
+						{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+						{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+						{
+							icon = " ",
+							key = "g",
+							desc = "Find Text",
+							action = ":lua Snacks.dashboard.pick('live_grep')",
+						},
+						{
+							icon = " ",
+							key = "r",
+							desc = "Recent Files",
+							action = ":lua Snacks.dashboard.pick('oldfiles')",
+						},
+						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+						{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					},
+				},
+			},
+		},
+	},
 }

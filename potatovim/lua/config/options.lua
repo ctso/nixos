e5d66345968fd3b86873e7e2b9ca60b0b723de9b
Chris Soyars <chris@soyars.com>
2025-12-30 22:34:14 -0800

    fix(potatovim): add terminal navigation bindings

--- potatovim/lua/config/options.lua
+++ potatovim/lua/config/options.lua
@@ -15,3 +15,13 @@ opt.termguicolors = true
 opt.undofile = true
 opt.undolevels = 10000
 opt.wrap = false
+
+local map = vim.keymap.set
+map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+map("t", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "Move to left window" })
+map("t", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "Move to bottom window" })
+map("t", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "Move to top window" })
+map("t", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "Move to right window" })

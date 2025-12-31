884e5bd4fc53c7ccf174e4fd8567d6ba6b1266b0
Chris Soyars <chris@soyars.com>
2025-12-30 20:58:54 -0800

    chore(potatovim): clean up snacks

--- potatovim/lua/plugins/snacks.lua
+++ potatovim/lua/plugins/snacks.lua
@@ -1,41 +1,46 @@
 return {
-	"folke/snacks.nvim",
-	lazy = false,
-	---@type snacks.Config
-	opts = {
-		bigfile = { enabled = true },
-		quickfile = { enabled = true },
-		indent = { enabled = true },
-		notifier = { enabled = true },
-		picker = {
-			enabled = true,
-			layout = { preset = "ivy" },
-		},
-		statuscolumn = {
-			enabled = true,
-		},
-	},
-	keys = {
-		{
-			"<leader><space>",
-			function()
-				Snacks.picker.smart()
-			end,
-			desc = "Smart Find Files",
-		},
-		{
-			"<leader>fg",
-			function()
-				Snacks.picker.grep()
-			end,
-			desc = "Find with Grep",
-		},
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-	},
+  "folke/snacks.nvim",
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    indent = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    input = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    toggle = { enabled = true },
+    words = { enabled = true },
+    picker = {
+      enabled = true,
+      layout = { preset = "ivy" },
+    },
+    statuscolumn = {
+      enabled = true,
+    },
+  },
+  keys = {
+    {
+      "<leader><space>",
+      function()
+        Snacks.picker.smart()
+      end,
+      desc = "Smart Find Files",
+    },
+    {
+      "<leader>fg",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "Find with Grep",
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+  },
 }

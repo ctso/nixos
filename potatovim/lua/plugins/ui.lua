974ba47f6adb0bb7f8213064a79acbca2ca1d158
Chris Soyars <chris@soyars.com>
2025-12-30 20:52:03 -0800

    chore(potatovim): move to ui.lua

---
+++ potatovim/lua/plugins/ui.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    opts = function()
+      return {
+        options = {
+          theme = "auto",
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff" },
+          lualine_c = {
+            "filename",
+            {
+              "diagnostics",
+              symbols = {
+                error = " ",
+                warn  = " ",
+                info  = " ",
+                hint  = " ",
+              },
+            },
+            { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          },
+          lualine_x = {
+            {
+              "diff",
+              symbols = {
+                added    = " ",
+                modified = " ",
+                removed  = " ",
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
+          },
+          lualine_y = { "progress", "location" },
+          lualine_z = {
+            function()
+              return " " .. os.date("%R")
+            end
+          },
+        },
+      };
+    end,
+  },
+  {
+    "nvim-mini/mini.icons",
+    lazy = true,
+    opts = {
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
+}

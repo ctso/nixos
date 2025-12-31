400f4ee37304783840674ae8cdcd80abf17c26f7
Chris Soyars <chris@soyars.com>
2025-12-30 22:58:06 -0800

    feat(potatovim): enable blink

--- potatovim/lua/plugins/code.lua
+++ potatovim/lua/plugins/code.lua
@@ -1,35 +1,89 @@
 return {
-  -- automatically add matching pairs, ie () [] ""
-  {
-    "nvim-mini/mini.pairs",
-    event = "VeryLazy",
-    opts = {
-      modes = { insert = true, command = true, terminal = false },
-      -- skip autopair when next character is one of these
-      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-      -- skip autopair when the cursor is inside these treesitter nodes
-      skip_ts = { "string" },
-      -- skip autopair when next character is closing pair
-      -- and there are more closing pairs than opening pairs
-      skip_unbalanced = true,
-      -- better support for markdown
-      markdown = true,
-    },
-  },
+	-- automatically add matching pairs, ie () [] ""
+	{
+		"nvim-mini/mini.pairs",
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better support for markdown
+			markdown = true,
+		},
+	},
 
-  -- Enhances native comments, more language support, relaxed rules, etc
-  { "folke/ts-comments.nvim" },
+	-- Enhances native comments, more language support, relaxed rules, etc
+	{ "folke/ts-comments.nvim" },
 
-  -- Better support for editing Neovim configs
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    cmd = "LazyDev",
-    opts = {
-      library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "snacks.nvim", words = { "Snacks" } },
-      },
-    },
-  },
+	-- Better support for editing Neovim configs
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		cmd = "LazyDev",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+			},
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		version = "*",
+		opts_extend = {
+			"sources.completion.enabled_providers",
+			"sources.compat",
+			"sources.default",
+		},
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			{
+				"saghen/blink.compat",
+				optional = true,
+				opts = {},
+				version = "*",
+			},
+		},
+		opts = {
+			snippets = {
+				preset = "default",
+			},
+
+			appearance = {
+				use_nvim_cmp_as_default = false,
+				nerd_font_variant = "mono",
+			},
+
+			completion = {
+				accept = {
+					auto_brackets = { enabled = true },
+				},
+				menu = {
+					draw = { treesitter = { "lsp" } },
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 200,
+				},
+				ghost_text = {
+					enabled = true,
+				},
+			},
+
+			sources = {
+				compat = {},
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+
+			keymap = {
+				preset = "enter",
+				["<C-y>"] = { "select_and_accept" },
+			},
+		},
+	},
 }

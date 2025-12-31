a2f5e166a3eaa619a9ccc50b10c0811ece2b4ade
Chris Soyars <chris@soyars.com>
2025-12-30 23:41:20 -0800

    fix(vim): notify on lsp errors

--- potatovim/lua/plugins/codestyle.lua
+++ potatovim/lua/plugins/codestyle.lua
@@ -5,7 +5,10 @@ return {
 		opts = {
 			format_on_save = {
 				timeout_ms = 500,
+				lsp_format = "never",
 			},
+			notify_on_error = true,
+			notify_no_formatters = true,
 			formatters_by_ft = {
 				lua = { "stylua" },
 			},

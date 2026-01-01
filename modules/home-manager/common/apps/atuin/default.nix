58e0b07de51c0fe55fd9b4c7495a666b3e641fea
Chris Soyars <chris@soyars.com>
2025-12-31 23:34:13 -0800

    chore(atuin): hook up to server

--- modules/home-manager/common/apps/atuin/default.nix
+++ modules/home-manager/common/apps/atuin/default.nix
@@ -4,6 +4,11 @@
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://atuin.c9s.io";
+    };
   };
 
   catppuccin.atuin = {

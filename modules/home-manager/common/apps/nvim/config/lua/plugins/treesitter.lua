30c304503ccfc0d18dea7716592b01026494b859
Chris Soyars <chris@soyars.com>
2026-01-08 21:01:29 -0800

    fix(vim): use newer treesitter

--- modules/home-manager/common/apps/nvim/config/lua/plugins/treesitter.lua
+++ modules/home-manager/common/apps/nvim/config/lua/plugins/treesitter.lua
@@ -3,15 +3,53 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
 		build = ":TSUpdate",
-		cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
-		opts_extend = { "ensure_installed" },
-		opts = {
-			auto_install = true,
-			ensure_installed = "all",
-			indent = { enable = true },
-			highlight = { enable = true },
-			folds = { enable = true },
-		},
+		config = function()
+			local ts = require("nvim-treesitter")
+			ts.setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+
+			-- Install common parsers automatically
+			ts.install({
+				"bash",
+				"css",
+				"diff",
+				"dockerfile",
+				"go",
+				"gomod",
+				"gosum",
+				"html",
+				"javascript",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"nix",
+				"python",
+				"regex",
+				"rust",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+				"php",
+			})
+
+			-- Enable treesitter highlighting for all filetypes
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					-- Only start if a parser exists for this filetype
+					if pcall(vim.treesitter.start) then
+						-- Enable treesitter-based indentation
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",

f7b7ebc9ae9bc2de8378ff26c7f5491ca4093602
Chris Soyars <chris@soyars.com>
2026-01-11 12:08:03 -0800

    fix(tmux): do not allow window renaming

--- modules/home-manager/common/apps/tmux/default.nix
+++ modules/home-manager/common/apps/tmux/default.nix
@@ -8,6 +8,12 @@
       sensible
       vim-tmux-navigator
     ];
+    extraConfig = ''
+      # Don't let programs rename windows
+      set-option -g allow-rename off
+      # Don't automatically rename windows based on running process
+      set-option -g automatic-rename off
+    '';
   };
 
   catppuccin.tmux = {
